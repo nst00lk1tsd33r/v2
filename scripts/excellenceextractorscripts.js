64e720209dcf30d976bbe9e397162ef8f4eae7d0
Darel <darelmiranda477@gmail.com>
2026-04-04 18:23:15 -0400

    Update excellenceextractorscripts.js

--- scripts/excellenceextractorscripts.js
+++ scripts/excellenceextractorscripts.js
@@ -63,6 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let prefix = '';
     if (linkCount >= 2) {
       prefix = '<p>No match found. Please see search queries below.</p>';
+      &nbsp;
     } else if (linkCount === 1) {
       prefix = '<p>No match found. Please see search query below.</p><p>&nbsp;</p>';
     }
